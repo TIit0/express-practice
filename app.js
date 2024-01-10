5ad7bf6de468d4c3d7a9e6f983bda6f2817c0794
My Name <viverlin7@gmail.com>
2024-01-10 13:46:27 -0600

    getting resources example

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+// get all files
+
+const homepage = readFileSync("./navbar-app/index.html");
+const styles = readFileSync("./navbar-app/styles.css");
+const js = readFileSync("./navbar-app/browser-app.js");
+const logo = readFileSync("./navbar-app/logo.svg");
+
 
 const server = http.createServer((req, res) => {
 
     console.log(req.url)
     if (req.url === "/") {
         res.writeHead(200, { "content-type": "text/html" }); /* mime type */
-        res.write("<h1>Home page</h1>")
+        res.write(homepage)
     } else if (req.url === "/about") {
         res.writeHead(200, { "content-type": "text/html" }); /* mime type */
         res.write("<h1>about</h1>")
+    }
+    else if (req.url === "/styles.css") {
+        res.writeHead(200, { "content-type": "text/css" }); /* mime type */
+        res.write(styles)
+    }
+    else if (req.url === "/browser-app.js") {
+        res.writeHead(200, { "content-type": "text/javascript" }); /* mime type */
+        res.write(js)
+    }
+    else if (req.url === "/logo.svg") {
+        res.writeHead(200, { "content-type": "image/svg+xml" }); /* mime type */
+        res.write(logo)
     } else {
         res.writeHead(404, { "content-type": "text/html" }); /* mime type */
         res.write("<h1>404</h1>")
     }
 
-    res.end()
+    res.end();
 })
 
-server.listen(5000)
+server.listen(5000);
