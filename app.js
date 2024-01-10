e3be664d131e61f3d2b850ca96befb144af15d2f
My Name <viverlin7@gmail.com>
2024-01-10 13:14:48 -0600

    route example response

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {"content-type": "text/html"}); /* mime type */
-    res.write("<h1>you did it</h1>")
+
+    console.log(req.url)
+    if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text/html" }); /* mime type */
+        res.write("<h1>Home page</h1>")
+    } else if (req.url === "/about") {
+        res.writeHead(200, { "content-type": "text/html" }); /* mime type */
+        res.write("<h1>about</h1>")
+    } else {
+        res.writeHead(404, { "content-type": "text/html" }); /* mime type */
+        res.write("<h1>404</h1>")
+    }
+
     res.end()
 })
 
