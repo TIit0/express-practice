a51c21f4215c3789e00f00a957951b89e1d4eeb2
My Name <viverlin7@gmail.com>
2024-01-22 16:25:34 -0600

    simple post example

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
-let {people} = require("./data")
 
-app.get("/", logger,(req, res) => {
-    return res.status(200).send("<h1>Home page</h1>")
+app.use(express.static("./methods-public"))
+app.use("/login", express.urlencoded({}))
+
+app.get("/", (req, res) => {
+    return res.status(200).send("Home")
 });
 
-app.get("/api/people", (req, res) => {
-    return res.status(201).json({status: "sucess", data: people})
+app.post("/login", (req, res) => {
+    const { name } = req.body;
+
+    if (name) {
+        return res.status(201).send(`<h1>Welcome: ${name}</h1>`)
+    }
+    return res.status(401).send("Empty field / Unathorized")
 })
 
 app.listen(5000, () => {
-    console.log("Listening on port 5000...")
-})
+    console.log("Listening on post 5000...")
+});
