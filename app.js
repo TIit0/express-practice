dfcdf3e48b33466ca919c8ab6b5e7d24b86e1b7c
My Name <viverlin7@gmail.com>
2024-01-22 18:45:24 -0600

    javascript example

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const app = express();
+let { people } = require("./data")
 
-app.use(express.static("./methods-public"))
-app.use("/login", express.urlencoded({}))
+app.use(express.static("./methods-public"));
 
-app.get("/", (req, res) => {
-    return res.status(200).send("Home")
+//parse urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+
+
+app.get("/api/people", (req, res) => {
+    return res.status(201).json({ success: true, data: people })
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/people", (req, res) => {
     const { name } = req.body;
-
-    if (name) {
-        return res.status(201).send(`<h1>Welcome: ${name}</h1>`)
+    if(!name) {
+        return res.status(400).json({success: false, msg: "please insert a name"})
+    } else {
+        return res.status(201).json({success: true, person: name})
     }
-    return res.status(401).send("Empty field / Unathorized")
-})
+});
 
 app.listen(5000, () => {
-    console.log("Listening on post 5000...")
+    console.log("Listening on port 5000...")
 });
