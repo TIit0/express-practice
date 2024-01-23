d36e1a461f96a3f87677708e64abaadb0681f595
My Name <viverlin7@gmail.com>
2024-01-23 13:25:12 -0600

    put example

--- app.js
+++ app.js
@@ -16,12 +16,52 @@ app.get("/api/people", (req, res) => {
 
 app.post("/api/people", (req, res) => {
     const { name } = req.body;
-    if(!name) {
-        return res.status(400).json({success: false, msg: "please insert a name"})
+    if (!name) {
+        return res.status(400).json({ success: false, msg: "please insert a name" })
     } else {
-        return res.status(201).json({success: true, person: name})
+        return res.status(201).json({ success: true, person: name })
     }
 });
+
+/* 
+---  My attempt at blind put method ---
+
+app.put("/api/people/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(req.body)
+    if (!id || !req.body) {
+        return res.status(401).json({
+            success: false,
+            msg: "bad request"
+        })
+    }
+
+    const newPeople = people.map(
+        person => person.id == id ? person = { id: Number(id), ...req.body } : person = person
+    );
+    return res.status(201).json({
+        success: true,
+        data: newPeople
+    });
+});
+
+*/
+
+app.put("api/people/:id", (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const person = people.find(person => person.id === Number(id));
+
+    if (!person) return res.status(400).json({ success: false, msg: `no person with id:${id}` });
+
+    const newPeople = people.map(person => {
+        if (person.id === Number(id)) {
+            person.name = name;
+        }
+    });
+
+    res.status(200).json({success: true, data: newPeople})
+})
 
 app.listen(5000, () => {
     console.log("Listening on port 5000...")
